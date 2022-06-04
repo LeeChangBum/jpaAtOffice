a5df19d3b0cdef91866228973654a8c5b1cc83a6
CBLee <lcb4843@naver.com>
2022-06-04 17:01:50 +0900

    pushed well

--- core/core/src/main/java/hello/core/order/OrderServiceImp.java
+++ core/core/src/main/java/hello/core/order/OrderServiceImp.java
@@ -5,16 +5,23 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
+@Component
+//@RequiredArgsConstructor//final이 붙은 애들로 생성자를 만들어줌
 public class OrderServiceImp implements OrderService{
 
     private final MemberRepository memberRepository;
-
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceImp(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
+    public OrderServiceImp(MemberRepository memberRepository, DiscountPolicy disDiscountPolicy) { //->이걸 @RequiredArgsConstructor가 해줌
+       this.memberRepository = memberRepository;
+        this.discountPolicy = disDiscountPolicy;
     }
 
     @Override
